88b2b254afe25cf6da41aaecc07c1006ebfb5222
MuaazNasir <mohidmuaaz120@gmail.com>
2023-10-15 15:13:57 +0500

    first commit

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-import pandas as pd
-import requests
-URL = "https://hub.dummyapis.com/employee?noofRecords=100&idStarts=100"
-r = requests.get(URL)
-r.raise_for_status()
-if r.status_code == 200 and r.text :
-    rData = r.json()
-    df = pd.DataFrame(rData)
+def getInps() -> dict[str,int] :
+    num1 = int(input("\nenter 1st number\t"))
+    num2 = int(input("\nenter 2nd number\t"))
+    operation = input("\nwhich operation you want to perform ?\nAddition\nSubtraction\nMultiplication\nDivision\nchoose one :\t").lower()
     
-    file_name = "emloyees.xlsx"
-    writer = pd.ExcelWriter(file_name,engine="xlsxwriter")
-    
-    df.to_excel(writer,sheet_name="Employees Data",index=False)
-    writer._save()
-    
-    print(f"created a file name '{file_name}'")
-else :
-    print("error occured")
+    return {"num1":num1,"num2":num2,"operation":operation}
+
+class Calculator :
+    def __init__(self,num1:int,num2:int) :
+        self.num1 = num1
+        self.num2 = num2
+    add = lambda self : self.num1 + self.num2
+    sub = lambda self : self.num1 - self.num2
+    mult = lambda self : self.num1 * self.num2
+    div = lambda self : self.num1 / self.num2
+
+
+
+def main() :
+    inps = getInps()
+    calc = Calculator(inps["num1"],inps["num2"])
+    play = 1
+    while play :
+        if inps["operation"] == "1" or inps["operation"] == "addition" :
+            out = calc.add()
+            print(f"your result is {out}")
+        elif inps["operation"] == "2" or inps["operation"] == "subtraction" :
+            out = calc.sub()
+            print(f"your result is {out}")
+        elif inps["operation"] == "3" or inps["operation"] == "multiplication" :
+            out = calc.mult()
+            print(f"your result is {out}")
+        elif inps["operation"] == "4" or inps["operation"] == "division" :
+            out = calc.div()
+            print(f"your result is {out}")
+        play = int(input("do you want to play again? (1(yes) / 0(no))"))
+        exit(not play)
+
+main()
